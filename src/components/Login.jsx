f759d39d4875bf2f9cc9b90139919196e096c0b2
Trungvo93 <thanhtrungvo1993@gmail.com>
2023-02-03 17:09:29 +0700

    .

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import logo_library from "../images/logo-library.png";
 import "../css/Login.scss";
+import {
+  TextField,
+  Box,
+  FormControl,
+  InputLabel,
+  InputAdornment,
+  IconButton,
+  OutlinedInput,
+  Button,
+} from "@mui/material";
+import { Visibility, VisibilityOff } from "@mui/icons-material";
 const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   return (
     <Container className="mt-5">
       <Row>
@@ -14,8 +33,43 @@ const Login = () => {
           </div>
         </Col>
         <Col md={6} className="login-form my-5">
-          <div>
+          <div className="shadow p-5 rounded">
             <h3 className="text-center fw-bold">Login Account</h3>
+            <Box>
+              <TextField
+                label="Username"
+                variant="outlined"
+                margin="normal"
+                fullWidth
+              />
+              <FormControl variant="outlined" fullWidth margin="normal">
+                <InputLabel>Password</InputLabel>
+                <OutlinedInput
+                  type={showPassword ? "text" : "password"}
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        onMouseDown={handleMouseDownPassword}
+                        edge="end">
+                        {showPassword ? <VisibilityOff /> : <Visibility />}
+                      </IconButton>
+                    </InputAdornment>
+                  }
+                  label="Password"
+                />
+              </FormControl>
+              <Button
+                variant="contained"
+                sx={{
+                  m: "10px auto 0",
+                  display: "block",
+                  p: "10px 20px",
+                }}>
+                SIGN IN
+              </Button>
+            </Box>
           </div>
         </Col>
       </Row>

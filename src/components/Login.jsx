6b0ef623c554a4f8b3b8a9a145d57aca10991894
Trungvo93 <thanhtrungvo1993@gmail.com>
2023-02-09 16:25:19 +0700

    .

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import logo_library from "../images/logo-library.png";
@@ -26,6 +27,7 @@ const Login = () => {
   const handleMouseDownPassword = (event) => {
     event.preventDefault();
   };
+
   return (
     <Container>
       <Row className={`${loginStyles.main}`}>
--- src/components/RecoveryPass.jsx
+++ src/components/RecoveryPass.jsx
@@ -10,11 +10,13 @@ import {
   DialogActions,
   DialogContent,
   DialogContentText,
+  Alert,
+  Snackbar,
 } from "@mui/material";
 const RecoveryPass = () => {
   const navigate = useNavigate();
   const [open, setOpen] = useState(false);
-
+  const [openAlert, setOpenAlert] = useState(false);
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -25,6 +27,14 @@ const RecoveryPass = () => {
   const handleLogin = () => {
     navigate("/");
   };
+
+  const handleShowAlert = () => {
+    setOpenAlert(true);
+  };
+
+  const handleCloseAlert = (event, reason) => {
+    setOpenAlert(false);
+  };
   return (
     <Container>
       <Row className="main">
@@ -32,6 +42,18 @@ const RecoveryPass = () => {
           lg={6}
           className={`${recoveryStyles["recovery-form"]} my-5 mx-auto `}>
           <div className="shadow p-5 rounded">
+            <Button
+              variant="contained"
+              sx={{
+                m: "10px auto 0",
+                display: "block",
+                p: "10px 20px",
+              }}
+              onClick={() => {
+                handleShowAlert();
+              }}>
+              Test
+            </Button>
             <h3 className="text-center fw-bold">Forgot Password</h3>
             <Box>
               <TextField
@@ -49,6 +71,7 @@ const RecoveryPass = () => {
               />
               <Button
                 variant="contained"
+                fullWidth
                 sx={{
                   m: "10px auto 0",
                   display: "block",
@@ -63,17 +86,36 @@ const RecoveryPass = () => {
       </Row>
       <Dialog open={open} onClose={handleClose}>
         <DialogContent>
-          <DialogContentText>
+          <DialogContentText className="text-success fw-bold text-uppercase">
             New password has been sent to email
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleLogin}>Sign in</Button>
-          <Button onClick={handleClose} autoFocus>
+          <Button
+            onClick={handleLogin}
+            variant="contained"
+            autoFocus
+            color="success">
+            Sign in
+          </Button>
+          <Button
+            onClick={handleClose}
+            sx={{
+              color: "gray",
+            }}>
             Stay here
           </Button>
         </DialogActions>
       </Dialog>
+      <Snackbar
+        open={openAlert}
+        autoHideDuration={4000}
+        onClose={handleCloseAlert}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}>
+        <Alert severity="error" variant="filled" sx={{ width: "100%" }}>
+          Username or Email not correct !
+        </Alert>
+      </Snackbar>
     </Container>
   );
 };

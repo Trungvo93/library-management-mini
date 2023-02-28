88ac90386feb25e2031fb062717e9e773c60568a
Trungvo93 <thanhtrungvo1993@gmail.com>
2023-02-28 16:53:18 +0700

    Làm lẹ cái tay lên q q!
    asd

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,26 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Outlet, useNavigate, Link } from "react-router-dom";
+import { Outlet, useNavigate, NavLink } from "react-router-dom";
 import { fetchUsers } from "../redux/usersSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useCookies } from "react-cookie";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import layoutStyles from "../css/Layout.module.scss";
 import logoLibrary from "../images/logo-library.png";
-
-import {
-  Collapse,
-  ListItemText,
-  ListItemIcon,
-  ListItemButton,
-  List,
-  ListSubheader,
-  Avatar,
-} from "@mui/material";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import DraftsIcon from "@mui/icons-material/Drafts";
-import ExpandLess from "@mui/icons-material/ExpandLess";
-import ExpandMore from "@mui/icons-material/ExpandMore";
-import StarBorder from "@mui/icons-material/StarBorder";
+import { Collapse, ListItemButton, List, Avatar } from "@mui/material";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import CachedOutlinedIcon from "@mui/icons-material/CachedOutlined";
+import LibraryBooksOutlinedIcon from "@mui/icons-material/LibraryBooksOutlined";
+import GroupOutlinedIcon from "@mui/icons-material/GroupOutlined";
 const Layout = () => {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.users);
@@ -28,8 +19,15 @@ const Layout = () => {
   const navigate = useNavigate();
   const [loginUser, setLoginUser] = useState({});
   const [cookies, setCookie] = useCookies();
-  const [open, setOpen] = useState(true);
-
+  const [open, setOpen] = useState(false);
+  let activeStyle = {
+    color: "white",
+    backgroundColor: "#2A2BE7",
+    fontWeight: "bold",
+  };
+  let noneActiveStyle = {
+    color: `gray`,
+  };
   const handleClick = () => {
     setOpen(!open);
   };
@@ -51,39 +49,19 @@ const Layout = () => {
   const handleGotoHome = () => {
     navigate("/index/dashboard");
   };
+  const handleLogout = () => {
+    navigate("/");
+  };
   return (
     <Container fluid>
       <Row>
-        {/* <Col
-          md={2}
-          className={`${layoutStyles.menu} p-3 d-flex flex-column gap-4`}>
-          <div className={`${layoutStyles.logo} d-flex gap-3 mt-3`}>
-            <img src={logoLibrary} alt="" />
-            <span className="fw-bold d-none d-xl-block">Librarian</span>
-          </div>
-
-          <div className="d-flex align-items-center gap-3 ">
-            {loginUser ? (
-              <>
-                <Avatar alt="Remy Sharp" src={loginUser.avatar} />
-                <div>
-                  <span className="fw-bold d-none d-xl-block">
-                    {loginUser.name}
-                  </span>
-                </div>
-              </>
-            ) : (
-              ""
-            )}
-          </div>
-        </Col> */}
         <Col md={2} className="p-0">
           <List
             className={`${layoutStyles.menu}`}
             sx={{ width: "100%" }}
             component="nav">
-            <ListItemButton onClick={handleGotoHome} className="mt-3">
-              <div className={` d-flex gap-3 mt-3`}>
+            <ListItemButton onClick={handleGotoHome} className="my-3">
+              <div className={` d-flex gap-3 my-3`}>
                 <img
                   src={logoLibrary}
                   alt=""
@@ -117,11 +95,72 @@ const Layout = () => {
             <Collapse in={open} timeout="auto" unmountOnExit>
               <List
                 component="div"
-                className="me-0 d-flex justify-content-around gap-2 align-items-center">
-                <p className={`m-0 ${layoutStyles.hoverLink}`}>Edit</p>
-                <p className={`m-0 ${layoutStyles.hoverLink}`}>Logout</p>
+                className="me-0 d-flex gap-2 align-items-center">
+                <p className={`m-0 ${layoutStyles.hoverLink} w-50 text-center`}>
+                  Edit
+                </p>
+                <p
+                  className={`m-0 ${layoutStyles.hoverLink} w-50 text-center`}
+                  onClick={handleLogout}>
+                  Logout
+                </p>
               </List>
             </Collapse>
+            <hr />
+
+            <ListItemButton className="p-0">
+              <NavLink
+                to="dashboard"
+                className="text-decoration-none w-100 h-100 p-3"
+                style={({ isActive }) =>
+                  isActive ? activeStyle : noneActiveStyle
+                }>
+                <div className={` d-flex gap-3 align-items-center w-100 h-100`}>
+                  <GridViewOutlinedIcon />
+                  <p className="d-none d-xl-block m-0 w-100 ">Dashboard</p>
+                </div>
+              </NavLink>
+            </ListItemButton>
+            <ListItemButton className="p-0">
+              <NavLink
+                to="libraryloan"
+                className="text-decoration-none w-100 h-100 p-3"
+                style={({ isActive }) =>
+                  isActive ? activeStyle : noneActiveStyle
+                }>
+                <div className={` d-flex gap-3 align-items-center`}>
+                  <CachedOutlinedIcon />
+                  <p className="d-none d-xl-block m-0 ">Library loan</p>
+                </div>
+              </NavLink>
+            </ListItemButton>
+            <ListItemButton className="p-0">
+              <NavLink
+                to="books"
+                className="text-decoration-none w-100 h-100 p-3"
+                style={({ isActive }) =>
+                  isActive ? activeStyle : noneActiveStyle
+                }>
+                <div className={` d-flex gap-3 align-items-center`}>
+                  <LibraryBooksOutlinedIcon />
+                  <p className="d-none d-xl-block m-0 ">Books</p>
+                </div>
+              </NavLink>
+            </ListItemButton>
+
+            <ListItemButton className="p-0">
+              <NavLink
+                to="members"
+                className="text-decoration-none w-100 h-100 p-3"
+                style={({ isActive }) =>
+                  isActive ? activeStyle : noneActiveStyle
+                }>
+                <div className={` d-flex gap-3 align-items-center`}>
+                  <GroupOutlinedIcon />
+                  <p className="d-none d-xl-block m-0 ">Members</p>
+                </div>
+              </NavLink>
+            </ListItemButton>
           </List>
         </Col>
         <Col md={7}>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import Login from "./components/Login";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Router from "./components/Router";
+import "bootstrap-icons/font/bootstrap-icons.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Router />);

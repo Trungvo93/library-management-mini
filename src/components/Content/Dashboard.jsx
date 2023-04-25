5f058d8aeeba91b2b967adf4e37faef5acec3fe2
Trungvo93 <thanhtrungvo1993@gmail.com>
2023-04-25 14:03:11 +0700

    .

--- src/components/Content/Dashboard.jsx
+++ src/components/Content/Dashboard.jsx
@@ -1,7 +1,28 @@
 import React from "react";
-
+import { Box, Grid } from "@mui/material";
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <Box marginTop="16px">
+      <Grid
+        Grid
+        container
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item md={3} sm={6} xs={12}>
+          <Box sx={{ backgroundColor: "green" }}>asdasd</Box>
+        </Grid>
+        <Grid item md={3} sm={6} xs={12}>
+          <Box sx={{ backgroundColor: "green" }}>asdasd</Box>
+        </Grid>
+        <Grid item md={3} sm={6} xs={12}>
+          <Box sx={{ backgroundColor: "green" }}>asdasd</Box>
+        </Grid>
+        <Grid item md={3} sm={6} xs={12}>
+          <Box sx={{ backgroundColor: "green" }}>asdasd</Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Dashboard;

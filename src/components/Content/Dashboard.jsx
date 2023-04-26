a7ebe84c3d8d412f7adbc97815f824129d558d60
Trungvo93 <thanhtrungvo1993@gmail.com>
2023-04-26 14:23:54 +0700

    .

--- src/components/Content/Dashboard.jsx
+++ src/components/Content/Dashboard.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-import { Box, Grid } from "@mui/material";
+import {
+  Box,
+  Grid,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+  Button,
+} from "@mui/material";
 const Dashboard = () => {
   return (
-    <Box marginTop="16px">
+    <Box marginTop="16px" marginX="16px">
       <Grid
         Grid
         container
         rowSpacing={1}
         columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
         <Grid item md={3} sm={6} xs={12}>
-          <Box sx={{ backgroundColor: "green" }}>asdasd</Box>
+          <Card sx={{ backgroundColor: "green" }}>
+            <Grid
+              container
+              direction="row"
+              justifyContent="space-between"
+              alignItems="center">
+              <Grid item>
+                <Typography variant="body1">ABC</Typography>
+                <Typography variant="body1">ABC</Typography>
+              </Grid>
+            </Grid>
+          </Card>
         </Grid>
         <Grid item md={3} sm={6} xs={12}>
           <Box sx={{ backgroundColor: "green" }}>asdasd</Box>
